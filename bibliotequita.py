e77c8f5436b96c37e0d1927027b063c978e8384b
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-01-15 12:43:04 -0500

    creando modulo y consumiendolo

---
+++ bibliotequita.py
@@ -0,0 +1,11 @@
+def ponerEnMayuscula(texto):
+    return texto.upper()
+
+def ponerEnMinuscula(texto):
+    return texto.lower()
+
+def ponerLetraCapitalizada(texto):
+    return texto.capitalize()
+
+def flipTexto(texto):
+    return texto.swapcase()
---
+++ usando-mi-libreria.py
@@ -0,0 +1,8 @@
+import bibliotequita
+
+miTexto = 'hola sOy goku'
+
+print(bibliotequita.ponerLetraCapitalizada(miTexto))
+print(bibliotequita.ponerEnMayuscula(miTexto))
+print(bibliotequita.ponerEnMinuscula(miTexto))
+print(bibliotequita.flipTexto(miTexto))
